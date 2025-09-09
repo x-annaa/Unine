96f4ba745d9ab48cb03490461b1c27d67cfff5a3
Unine <annaannamx@gmail.com>
2025-09-09 14:50:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-// 底部菜单 // 
-function showPage(page) {
-  // 隐藏所有页面
-  const pages = document.querySelectorAll('.page');
-  pages.forEach(p => p.classList.remove('active'));
 
-  // 显示对应页面
-  document.getElementById('page' + page).classList.add('active');
-}
