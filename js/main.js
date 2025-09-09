a643d85f90d90461d1daaacb60921592c749e92e
Unine <annaannamx@gmail.com>
2025-09-09 16:07:34 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //（ 1 ）// Supabase 初始化模板
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://your-project-ref.supabase.co';
-const SUPABASE_ANON_KEY = 'your-public-anon-key';
+const SUPABASE_URL = 'https://frilvfwifdvcssuynyyh.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZyaWx2ZndpZmR2Y3NzdXlueXloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0NDYyNjgsImV4cCI6MjA3MzAyMjI2OH0.JZHg88B9ywTwFaytALHE8mz7q72-Wc8lIj5aZ_YvFSs';
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // 公共方法
